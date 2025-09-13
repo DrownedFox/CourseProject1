fefd4b88c0f88f0e64fabb5d95552064864d714c
DrownedFox <23240122@turan-edu.kz>
2025-09-13 19:36:56 +0500

    Добавлен новый класс

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Просто пустой файл!");
+            Console.ReadKey();
+        }
+    }
+}
